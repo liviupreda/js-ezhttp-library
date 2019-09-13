883cb13d9c16a14a9fcacd6737e82f7c79331dbe
Liviu Preda <liviupreda@gmail.com>
2019-09-13 11:57:33 +0300

    add POST, PUT and DELETE methods

--- app.js
+++ app.js
@@ -1,10 +1,61 @@
 const http = new ezHTTP();
 
 // Get posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Create Data for POST/ PUT
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// POST request
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// PUT request (update post)
+// http.put('https://jsonplaceholder.typicode.com/posts/100', data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// DELETE posts
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
+  err,
+  response
+) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 });
--- ezhttp.js
+++ ezhttp.js
@@ -22,7 +22,48 @@ ezHTTP.prototype.get = function(url, callback) {
 };
 
 // POST
+ezHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+  // Set Content type
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function() {
+    // NULL is the error, similar to node
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 
 // PUT
+ezHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
+  // Set Content type
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  let self = this;
+  this.http.onload = function() {
+    // NULL is the error, similar to node
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 
 // DESTROY
+ezHTTP.prototype.delete = function(url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+  this.http.onload = function() {
+    if (self.http.status === 200) {
+      // NULL is the error, similar to node
+      callback(null, 'Post deleted');
+    } else {
+      callback('Error: ' + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
