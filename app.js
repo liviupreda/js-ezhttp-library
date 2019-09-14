dd6dec408538dd5710d9054f1c96926748f52ff4
Liviu Preda <liviupreda@gmail.com>
2019-09-14 07:49:45 +0300

    add ezhttp2.js; ES6 classes, promises, fetch

--- app.js
+++ app.js
@@ -1,61 +1,94 @@
-const http = new ezHTTP();
+// const http = new ezHTTP();
 
-// Get posts
-// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// // Get posts
+// // http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     console.log(posts);
+// //   }
+// // });
+
+// // Get single post
+// // http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     console.log(post);
+// //   }
+// // });
+
+// //Create Data for POST/ PUT
+// const data = {
+//   title: 'Custom Post',
+//   body: 'This is a custom post'
+// };
+
+// // POST request
+// // http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+// //   err,
+// //   post
+// // ) {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     console.log(post);
+// //   }
+// // });
+
+// // PUT request (update post)
+// // http.put('https://jsonplaceholder.typicode.com/posts/100', data, function(
+// //   err,
+// //   post
+// // ) {
+// //   if (err) {
+// //     console.log(err);
+// //   } else {
+// //     console.log(post);
+// //   }
+// // });
+
+// // DELETE posts
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
+//   err,
+//   response
+// ) {
 //   if (err) {
 //     console.log(err);
 //   } else {
-//     console.log(posts);
+//     console.log(response);
 //   }
 // });
 
-// Get single post
-// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+const http = new EzHTTP();
 
-//Create Data for POST/ PUT
+// Get users
+// http
+//   .get('https://jsonplaceholder.typicode.com/users') //returns a promise
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Create User Data
 const data = {
-  title: 'Custom Post',
-  body: 'This is a custom post'
+  name: 'Liv Preda',
+  username: 'lpreda',
+  email: 'lp@liviupreda.me'
 };
 
-// POST request
-// http.post('https://jsonplaceholder.typicode.com/posts', data, function(
-//   err,
-//   post
-// ) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+// POST data
+// http
+//   .post('https://jsonplaceholder.typicode.com/users', data) //returns a promise
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// PUT request (update post)
-// http.put('https://jsonplaceholder.typicode.com/posts/100', data, function(
-//   err,
-//   post
-// ) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log(post);
-//   }
-// });
+// PUT data (update user)
+// http
+//   .put('https://jsonplaceholder.typicode.com/users/2', data) //returns a promise
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-// DELETE posts
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
-  err,
-  response
-) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(response);
-  }
-});
+// Delete User
+http
+  .delete('https://jsonplaceholder.typicode.com/users/2') //returns a promise
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
---
+++ ezhttp2.js
@@ -0,0 +1,60 @@
+// EZ HTTP 2 using ES6 (classes, arrow functions) and Fetch API
+
+class EzHTTP {
+  // GET
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // POST
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // PUT
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // DESTROY
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(() => resolve('User deleted'))
+        .catch(err => reject(err));
+    });
+  }
+}
